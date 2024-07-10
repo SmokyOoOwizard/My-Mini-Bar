e19f187bdbfa1132362d332a8afd16e690d6d2fc
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-07-10 16:14:28 +0200

    Add reset timer system

---
+++ Assets/Scripts/Ecs/Systems/Update/ResetTimerSystem.cs
@@ -0,0 +1,27 @@
+﻿using Ecs.Components;
+using Ecs.Components.Timer;
+using Ecs.Core;
+using Leopotam.Ecs;
+
+namespace Ecs.Systems.Update
+{
+    public class ResetTimerSystem : IUpdateEcsSystem
+    {
+        private EcsFilter<TimerComponent, ResetTimerComponent> _timerFilter;
+
+        public void Run()
+        {
+            foreach (var entityId in _timerFilter)
+            {
+                var entity = _timerFilter.GetEntity(entityId);
+                if (entity.Has<DoneComponent>())
+                    entity.Del<DoneComponent>();
+
+                var timer = entity.Get<TimerComponent>().Value;
+                entity.Get<TimerLeftComponent>().Value = timer;
+                
+                entity.Del<ResetTimerComponent>();
+            }
+        }
+    }
+}
