ccec15f10f6b3b36806a9613800dc4ec2903fae2
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2024-07-10 16:51:54 +0200

    Add dotween extension

---
+++ Assets/Scripts/Utils/Dotween/MoveInLocalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using UnityEngine;
+
+namespace Utils.Dotween
+{
+    public static class MoveInLocalExtensions
+    {
+        public static TweenerCore<Vector3, Vector3, VectorOptions> DOMoveInTargetLocalSpace(this Transform transform, Transform target, Vector3 targetLocalEndPosition,float duration)
+        {
+            var t = DOTween.To(
+                () => transform.position - target.transform.position, // Value getter
+                x => transform.position = x + target.transform.position, // Value setter
+                targetLocalEndPosition, 
+                duration);
+            t.SetTarget(transform);
+            return t;
+        }
+    }
+}
